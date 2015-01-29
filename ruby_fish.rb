3a515eb8117559a9a213dc7559e94db12698d39b
zvezdi <zvezdi.dim@gmail.com>
2015-01-29 13:03:19 +0200

    Adds an animated fish - class Green. Changes the player's movements.

--- ruby_fish.rb
+++ ruby_fish.rb
@@ -7,22 +7,25 @@ class GameWindow < Gosu::Window
 
     @background_image = Gosu::Image.new(self, "media/underwater-background.jpg", true)
     @player = Player.new self
+    @green_fish = Green.new self
   end
 
   def update
-    @player.goBack if button_down? Gosu::KbLeft
-    @player.goStraight if button_down? Gosu::KbRight
+    @player.goLeft if button_down? Gosu::KbLeft
+    @player.goRight if button_down? Gosu::KbRight
     @player.goUp if button_down? Gosu::KbUp
     @player.goDown if button_down? Gosu::KbDown 
 
     close if button_down? Gosu::KbEscape
 
     @player.stayInside
+    @green_fish.update
   end
 
   def draw 
     @background_image.draw(0, 0, 0)
     @player.draw
+    @green_fish.draw
   end
 end
 
@@ -34,17 +37,19 @@ class Player
     @y = @window.height / 2
     @angle = 0
     @lookLeft = false
+    @size = 0.2
+    @velosity = 4
   end
 
-  def goStraight
+  def goRight
     @lookLeft = false
-    @x += 1
+    @x += @velosity
     @angle -= 0.52 if @angle > 0
     @angle += 0.52 if @angle < 0 
   end
 
   def goUp
-    @y -= 0.5
+    @y -= @velosity
     if !@lookLeft and @angle > -87 and @angle <= 90 
       @angle -= 0.5
     end
@@ -54,7 +59,7 @@ class Player
   end
 
   def goDown
-    @y += 0.5
+    @y += @velosity
     if !@lookLeft and @angle >= -90 and @angle < 87 
       @angle += 0.5
     end
@@ -63,9 +68,9 @@ class Player
     end
   end
 
-  def goBack
+  def goLeft
     @lookLeft = true
-    @x -= 1
+    @x -= @velosity
     @angle -= 0.52 if @angle > 0
     @angle += 0.52 if @angle < 0 
   end
@@ -77,15 +82,59 @@ class Player
 
   def toDraw
     if !@lookLeft
-      @image.draw_rot(@x, @y, 1, @angle, 0.5, 0.5, 0.2, 0.2)
+      @image.draw_rot(@x, @y, 1, @angle, 0.5, 0.5, @size, @size)
     end
     if @lookLeft
-      @image.draw_rot(@x, @y, 1, @angle, 0.5, 0.5, -0.2, 0.2)
+      @image.draw_rot(@x, @y, 1, @angle, 0.5, 0.5, -@size, @size)
     end
   end
 
   def draw
     toDraw
+  end
+end
+
+class Green
+  def initialize(window)
+    @window = window
+    @width = 256
+    @height = 256
+    @image = Gosu::Image.load_tiles @window, "media/green.png", 
+                                    @width, @height, true
+    @x = 0
+    @y = 0
+    @nextFrame = 0
+    @size = 0.6
+    @direction = :right
+    @frame = 0
+  end
+
+  def update
+    @frame += 1 if @nextFrame == 5
+    if @nextFrame < 5
+      @nextFrame += 1
+    elsif @nextFrame == 5
+      @nextFrame = 0
+    end
+        
+    case @direction
+      when :right   then @x += 3
+      when :left    then @x -= 3
+    end
+
+    @direction = :left if @x >= (@window.width - @width * @size) 
+    @direction = :right if @x <= 0
+  end
+
+  def draw
+    r = @frame % 6 + 12
+    l = @frame % 6 + 18
+    image = @image
+    case @direction
+      when :right  then image = @image[r]
+      when :left   then image = @image[l]
+    end
+    image.draw @x, @y, 1, @size, @size
   end
 end
 
